756c02e7cf2c322a38bf253db6093d839bd00b80
degui1 <gui.denez56@gmail.com>
2023-12-05 20:53:17 -0300

    Swagger - Documentação

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import { serve, setup } from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerApi from "./swaggerApi.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
-
+app.use("/api-docs", serve, setup(swaggerApi));
 export { app };
